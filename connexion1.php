f5f43ed5da340e2baff7be6bc1447c332aa7718f
dimitri-dks <83714134+dimitri-dks@users.noreply.github.com>
2021-05-17 13:05:57 +0200

    Add files via upload

---
+++ connexion1.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style1.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    <title>Projet Appli Web N7!</title>
+  </head>
+  <body>
+    <?php 
+    require_once('menu.php');
+    ?>
+
+    <div id="container">
+        <!-- zone de connexion -->
+        
+        <form action="verification.php" method="POST" id="form1">
+            <h1>Connexion</h1>
+            
+            <label><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required><br>
+
+            <label><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required><br>
+
+            Toujours pas de compte ?
+            <A href="inscription1.php"> Créer un compte ici </A><br>
+
+            <input type="submit" id='submit' value='LOGIN' >
+            <?php
+            if(isset($_GET['erreur'])){
+                $err = $_GET['erreur'];
+                if($err==1 || $err==2)
+                    echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+            }
+            ?>
+        </form>
+    </div>
+            <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+  </body>
+</html>
